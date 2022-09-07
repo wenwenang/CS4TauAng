05e258bdf6cd2fe20aa64561b279969a41e6d53f
mwlang <93103987+wenwenang@users.noreply.github.com>
2022-09-07 09:01:14 +0800

    Add files via upload

---
+++ Q1Ex1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author TAU
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String petname1 = "Puppsy";
+      String petname2 = "Morrie";
+      String petname3 = "Chewie";
+      
+      int petage1 = 11;
+      int petage2 = 2;
+      int petage3 = 4;
+      
+      String petbreed1 = "Shi Tzu";
+      String petbreed2 = "Japanese Spitz";
+      String petbreed3 = "Toy Poodle";
+      
+      String petcute1 = "very cute";
+      String petcute2 = "cute";
+      String petcute3 = "cute";
+      
+      System.out.print()
+      
+    }
+    
+}
